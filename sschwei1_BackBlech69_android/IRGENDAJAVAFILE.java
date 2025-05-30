9631b931786229dd226e492717ca090bfbeb0325
sschwei1 <24sebastian05@gmail.com>
2025-05-30 16:17:27 +0200

    Add some more stuff to java file

--- sschwei1_BackBlech69_android/IRGENDAJAVAFILE.java
+++ sschwei1_BackBlech69_android/IRGENDAJAVAFILE.java
@@ -4,5 +4,7 @@ package some.package;
 class Main {
     public static void Main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Some other changes.");
     }
 }
