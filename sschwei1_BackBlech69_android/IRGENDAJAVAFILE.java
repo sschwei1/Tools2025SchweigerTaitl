60bf0a94310d5020c215d60051be3b4d4f22402c
marlihu <s2410237007@students.fh-hagenberg.at>
2025-06-01 11:27:24 +0200

    changes to fix this infernally awful code that hurts my eyes & fix funny numbers

--- sschwei1_BackBlech69_android/IRGENDAJAVAFILE.java
+++ sschwei1_BackBlech69_android/IRGENDAJAVAFILE.java
@@ -1,23 +1,34 @@
-package some.package;
+package task01.sschwei1_BackBlech69_android;
 
+import java.util.Random;
 
-class Main {
-    public static void Main(String[] args) {
+class IRGENDAJAVAFILE {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
 
         System.out.println("Some other changes.");
+        theseAreOnlyRandomNumbers();
+    }
+
+    public static void thisIsANewMehtod() {
+        int x = 0;
+
+        while (x < 6969) {
+            System.out.println("Still less than 6969");
+            x++;
+        }
+
+        int EineZahl = 420;
+        EineZahl *= 187;
+        EineZahl += 69;
+    }
+
+
+    public static void theseAreOnlyRandomNumbers() {
+        Random random = new Random(441287210);
+        for (int i = 0; i < 10; i++)
+            System.out.print(random.nextInt(10) + " ");
     }
 }
 
-static public void thisIsANewMehtod(){
-    int x = 0;
 
-    while(x < 6969) {
-        System.out.println("Still less than 6969");
-        x++;
-    }
-
-    int EineZahl = 420;
-    EineZahl *= 100
-    EineZahl += 69
-}
